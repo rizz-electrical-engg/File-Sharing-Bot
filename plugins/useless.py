70585fff4bcd8b7fdbf72a89e5a2776910da2d0e
Rizz <123063244+rizz-electrical-engg@users.noreply.github.com>
2024-10-20 03:21:31 -0700

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -25,8 +25,3 @@ async def useless(_,message: Message):
 
 
 
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
