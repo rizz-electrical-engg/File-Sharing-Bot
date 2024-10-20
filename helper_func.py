54f1088575a894973762b31a6b8c9007df81ed5e
Rizz <123063244+rizz-electrical-engg@users.noreply.github.com>
2024-10-20 03:15:27 -0700

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -156,8 +156,3 @@ subscribed = filters.create(is_subscribed)
 
 
 
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
